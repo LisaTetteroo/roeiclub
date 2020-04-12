8c2f4b093e16fea6105501cd852db9de512042f7
Lisa Tetteroo <lisa.tetteroo.foto@gmail.com>
2020-04-12 19:33:39 +0200

    het lijkt een app, er kunnen palen en boten beheerd worden

--- BackEnd/src/main/java/nl/lisa/roeiclub/controller/VlootService.java
+++ BackEnd/src/main/java/nl/lisa/roeiclub/controller/VlootService.java
@@ -6,21 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 
 
-public class BootService {
+public class VlootService {
     @Autowired
     BootRepository br;
 
     @Autowired
     PalenRepository pr;
 
-    public void bootToevoegen(Boot b) {
+    public String bootToevoegen(Boot b) {
         System.out.println("in service bootToevoegen");
-        pr.save(b.getPalen());
-        br.save(b);
+        Palen p = b.getPalen();
+        Long paalId = p.getId();
+        Optional<Palen> palenoverzichtOptional = pr.findById(paalId);
+        if (palenoverzichtOptional.isPresent()) {
+            pr.save(p);
+            br.save(b);
+            return b.getNaam() + " is succesvol opgeslagen.";
+        } else {
+            return "Er is voor een nog niet bestaande paal gekozen, indien nog onbekend kies 1, of kies een paal die wel bestaat.";
+        }
     }
 
     public Iterable<Boot> botenInzien() {
@@ -43,6 +53,23 @@ public class BootService {
         }
     }
 
+    public String palenToevoegen(Palen p) {
+        System.out.println("in service palen toevoegen");
+        pr.save(p);
+        return "palen toevoegen succesvol";
+    }
+
+    public Iterable<Palen> palenInzien() {
+        Iterable<Palen> palenOverzicht = pr.findAll();
+        return palenOverzicht;
+    }
+
+    public void palenVerwijderen(Long id) {
+        System.out.println("in palenVerwijderen in VlootService");
+        pr.deleteById(id);
+    }
+
+    /*(
     public void probeersel () {
         System.out.println("bla");
         Boot b = new Boot();
@@ -51,4 +78,5 @@ public class BootService {
         b.setPalen(p);
         br.save(b);
     }
+    */
 }
